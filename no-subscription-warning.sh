22fd5a0087eb9f2125e23e1825110ab8277ee081
Andreas Steinel <A.Steinel@gmail.com>
2016-07-05 13:15:52 +0200

    Support PVE3 and PVE4

--- no-subscription-warning.sh
+++ no-subscription-warning.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-if [ $( grep -c "if (data.status !== 'Active') {" /usr/share/pve-manager/ext4/pvemanagerlib.js ) -gt 0 ]
+if [ -e /usr/share/pve-manager/ext4/pvemanagerlib.js ]
 then
-	echo "---------------[ Patching with No-Subscription-Removal-Patch ]----------------"
-	cd / && patch --no-backup-if-mismatch -l -p0 < /usr/share/pve-no-subscription/no-subscription-warning.patch
-	echo "------------------------------------------------------------------------------"
+    if [ $( grep -c "if (data.status !== 'Active') {" /usr/share/pve-manager/ext4/pvemanagerlib.js ) -gt 0 ]
+    then
+    	echo "---------------[ Patching with No-Subscription-Removal-Patch ]----------------"
+    	cd / && patch --no-backup-if-mismatch -l -p0 < /usr/share/pve-no-subscription/no-subscription-warning-ext4.patch
+    	echo "------------------------------------------------------------------------------"
+    fi
+else
+    if [ -e /usr/share/pve-manager/ext6/pvemanagerlib.js ]
+    then
+        if [ $( grep -c "if (data.status !== 'Active') {" /usr/share/pve-manager/ext6/pvemanagerlib.js ) -gt 0 ]
+        then
+        	echo "---------------[ Patching with No-Subscription-Removal-Patch ]----------------"
+        	cd / && patch --no-backup-if-mismatch -l -p0 < /usr/share/pve-no-subscription/no-subscription-warning-ext6.patch
+        	echo "------------------------------------------------------------------------------"
+        fi
+    fi
 fi
